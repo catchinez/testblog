761a97384cad601af7e89f5825debd39cce8f6ee
catchinez <winsaitw@gmail.com>
2022-05-24 19:09:24 +0300

    Added CRUD for Post, but not all

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -7,7 +7,15 @@
       <ul class="pt-3 nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <!-- Add icons to the links using the .nav-icon class
              with font-awesome or any other icon font library -->
-        
+        <li class="nav-item">
+          <a href="{{ route('admin.post.index') }}" class="nav-link">
+            <i class="nav-icon fa-solid fa-book-open"></i>
+            <p>
+              Posts
+            </p>
+          </a>
+        </li>
+
         <li class="nav-item">
           <a href="{{ route('admin.category.index') }}" class="nav-link">
             <i class="nav-icon fa-solid fa-bone"></i>
