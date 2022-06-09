289885f57c0281e307845882032ac67e78d55358
catchinez <winsaitw@gmail.com>
2022-06-10 01:40:27 +0300

    Added personal dashboard

---
+++ app/Http/Controllers/Personal/Main/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Personal\Main;
+
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+
+class IndexController extends Controller
+{
+    public function __invoke()
+    {
+        return view('personal.main.index');
+    }
+}
